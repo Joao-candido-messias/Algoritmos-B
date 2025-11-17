7a562c5b6428a6311c20c4256e86c99aad39c98f
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2025-11-17 22:49:25 +0000

    Feat(main.cpp, includes.h: Criação das funções do sistema.(Pronto para teste))

--- Trabalho Final/includes.h
+++ Trabalho Final/includes.h
@@ -1,5 +1,8 @@
+
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -14,33 +17,149 @@ typedef struct {
     string fabricante;
 } Degustacao;
 
+Degustacao vetor[TAM];
+int qtd = 0;
+
+//Função para conectar a base de dados e carregar os registros.
+int conectarBase(Degustacao vet[], int tam, string nomeArquivo){
+    ifstream arq(nomeArquivo);
+    if(!arq.is_open()){
+        return 0;
+    }
+
+    string linha;
+    int count = 0;
+
+    while(getline(arq, linha) && count < tam){
+        stringstream ss(linha);
+        string aux;
+
+        getline(ss, vet[count].dataDegustacao, ';');
+        getline(ss, vet[count].nomeAlimento, ';');
+        getline(ss, aux, ';');
+        vet[count].nota = stoi(aux);
+        getline(ss, vet[count].fabricante, ';');
+        getline(ss, vet[count].comentario, ';');
+
+        count++;
+    }
+
+    arq.close();
+    return count;
+}
+
+//Função para atualizar o arquivo base.
+void salvarArquivo(Degustacao vet[], int qtd, string nomeArquivo){
+    ofstream arq(nomeArquivo);
+
+    for(int i = 0; i < qtd; i++){
+        arq << vet[i].dataDegustacao << ";"
+            << vet[i].nomeAlimento << ";"
+            << vet[i].nota << ";"
+            << vet[i].fabricante << ";"
+            << vet[i].comentario;
+
+        if(i < qtd - 1)
+            arq << "\n";
+    }
+
+    arq.close();
+}
 
 //Função menu para interação com o sistema.
 void menu(){
-
+    cout << "\nMenu\n";
+    cout << "1 - Cadastrar\n";
+    cout << "2 - Listar\n";
+    cout << "3 - Remover\n";
+    cout << "4 - Pesquisar\n";
+    cout << "5 - Sair\n";
+    cout << "Opcao: ";
 }
 
 //Função para cadastrar uma degustação.
-void cadastrar(){
-    
+void cadastrar(Degustacao vet[], int &qtd, int tam, string nomeArquivo){
+    if(qtd >= tam){
+        return;
+    }
+
+    Degustacao d;
+
+    cin.ignore();
+    cout << "Data: ";
+    getline(cin, d.dataDegustacao);
+
+    cout << "Nome do alimento/bebida: ";
+    getline(cin, d.nomeAlimento);
+
+    cout << "Nota (1 a 5): ";
+    cin >> d.nota;
+
+    cin.ignore();
+    cout << "Fabricante: ";
+    getline(cin, d.fabricante);
+
+    cout << "Comentario: ";
+    getline(cin, d.comentario);
+
+    vet[qtd] = d;
+    qtd++;
+
+    salvarArquivo(vet, qtd, nomeArquivo);
 }
 
 //Função para listar as degustações já cadastradas.
-void listar(){
+void listar(Degustacao vet[], int qtd){
+    if(qtd == 0){
+        cout << "Nenhuma degustacao cadastrada.\n";
+        return;
+    }
 
-}
-
-//Função para remover/excluir uma das degustações já cadastradas.
-void remover(){
-
+    for(int i = 0; i < qtd; i++){
+        cout << "\nRegistro " << i + 1 << endl;
+        cout << "Data: " << vet[i].dataDegustacao << endl;
+        cout << "Alimento: " << vet[i].nomeAlimento << endl;
+        cout << "Nota: " << vet[i].nota << endl;
+        cout << "Fabricante: " << vet[i].fabricante << endl;
+        cout << "Comentario: " << vet[i].comentario << endl;
+    }
 }
 
 //Função para pesquisar uma das degustações já cadastradas.
-string pesquisar(){
+int pesquisar(Degustacao vet[], int qtd, string chave){
+    for(int i = 0; i < qtd; i++){
+        if(vet[i].nomeAlimento == chave){
+            return i;
+        }
+    }
+    return -1;
+}
 
+//Função para remover/excluir uma das degustações já cadastradas.
+void remover(Degustacao vet[], int &qtd, string nomeArquivo){
+    cin.ignore();
+    string chave;
+
+    cout << "Nome do alimento para remover: ";
+    getline(cin, chave);
+
+    int pos = pesquisar(vet, qtd, chave);
+
+    if(pos == -1){
+        cout << "Nao encontrado.\n";
+        return;
+    }
+
+    for(int i = pos; i < qtd - 1; i++){
+        vet[i] = vet[i + 1];
+    }
+
+    qtd--;
+    salvarArquivo(vet, qtd, nomeArquivo);
 }
 
 //Função com "Return 0; E mensagem de despedida do sistema.
 void sair(){
-    cout << "Bye :)" << endl;
+    cout << "Bye :)\n";
 }
+
--- Trabalho Final/main.cpp
+++ Trabalho Final/main.cpp
@@ -5,8 +5,58 @@
 using namespace std;
 
 int main(){
-    cout << "Hello Word bem Kawai.";
-    sair();
+    string nomeArquivo = "baseDados.csv";
+
+    qtd = conectarBase(vetor, TAM, nomeArquivo);
+
+    int opc;
+
+    do {
+        menu();
+        cin >> opc;
+
+        switch(opc){
+            case 1:
+                cadastrar(vetor, qtd, TAM, nomeArquivo);
+                break;
+
+            case 2:
+                listar(vetor, qtd);
+                break;
+
+            case 3:
+                remover(vetor, qtd, nomeArquivo);
+                break;
+
+            case 4: {
+                cin.ignore();
+                string chave;
+                cout << "Pesquisar por nome do alimento: ";
+                getline(cin, chave);
+
+                int pos = pesquisar(vetor, qtd, chave);
+
+                if(pos == -1){
+                    cout << "Nao encontrado.\n";
+                } else {
+                    cout << "Data: " << vetor[pos].dataDegustacao << endl;
+                    cout << "Alimento: " << vetor[pos].nomeAlimento << endl;
+                    cout << "Nota: " << vetor[pos].nota << endl;
+                    cout << "Fabricante: " << vetor[pos].fabricante << endl;
+                    cout << "Comentario: " << vetor[pos].comentario << endl;
+                }
+                break;
+            }
+
+            case 5:
+                sair();
+                break;
+
+            default:
+                cout << "Opcao invalida.\n";
+        }
+
+    } while(opc != 5);
+
     return 0;
 }
-   
