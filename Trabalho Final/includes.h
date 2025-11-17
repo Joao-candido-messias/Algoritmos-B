9f9f08b5617022b81bbb934ddaf16b7397f1e488
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2025-11-17 22:18:41 +0000

    docs(includes.h): Comentarios descritivos para as funções do sistema.

--- Trabalho Final/includes.h
+++ Trabalho Final/includes.h
@@ -5,6 +5,7 @@ using namespace std;
 
 #define TAM 1000
 
+//Struct de Degustação utilizada no sistema.
 typedef struct {
     string dataDegustacao;
     string comentario;
@@ -14,27 +15,32 @@ typedef struct {
 } Degustacao;
 
 
-
+//Função menu para interação com o sistema.
 void menu(){
 
 }
 
+//Função para cadastrar uma degustação.
 void cadastrar(){
     
 }
 
+//Função para listar as degustações já cadastradas.
 void listar(){
 
 }
 
+//Função para remover/excluir uma das degustações já cadastradas.
 void remover(){
 
 }
 
+//Função para pesquisar uma das degustações já cadastradas.
 string pesquisar(){
 
 }
 
+//Função com "Return 0; E mensagem de despedida do sistema.
 void sair(){
 
 }
