8d79bf0a42860441143ec2bd3c3085bd0103bf1d
João Cândido Messias <159557724+Joao-candido-messias@users.noreply.github.com>
2025-11-17 22:13:38 +0000

    Feat(includes.h): Estrutura basica das funções.

---
+++ Trabalho Final/includes.h
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define TAM 1000
+
+typedef struct {
+    string dataDegustacao;
+    string comentario;
+    int nota;
+    string nomeAlimento;
+    string fabricante;
+} Degustacao;
+
+
+
+void menu(){
+
+}
+
+void cadastrar(){
+    
+}
+
+void listar(){
+
+}
+
+void remover(){
+
+}
+
+string pesquisar(){
+
+}
+
+void sair(){
+
+}
---
+++ Trabalho Final/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << "Hello Word bem Kawai.";
+    return 0;
+}
+   
